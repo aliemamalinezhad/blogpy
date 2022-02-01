399c36ed3c7af82ea2cd706164e5faa8e7afb4c8
ali <aliemamalinezhad@gmail.com>
2022-02-02 02:08:35 +0330

    Create postgressql on docker

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'blogpy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT':'5432',
+        
     }
 }
 
@@ -113,7 +118,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
